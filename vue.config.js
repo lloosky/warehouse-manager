4cb77ee7ccd6334e2999f2bcf455b050fb5782ea
Pawel Switalla <pawel.switalla@gmail.com>
2019-10-29 13:52:50 +0100

    Cache busting, test #11

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/manager/' : '/',
-  pwa: {
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      swSrc: 'public/service-worker.js'
-    }
-  }
+  publicPath: process.env.NODE_ENV === 'production' ? '/manager/' : '/'
 };
