21d6188c97f98a23d947a642345a71af15cff29f
lloosky <karolswitalla@gmail.com>
2019-10-30 12:08:51 +0100

    added transitions

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <div id="application">
     <app-header v-if="$route.meta.hideNavigation"></app-header>
-    <router-view></router-view>
+    <transition name="slide" mode="out-in">
+      <router-view></router-view>
+    </transition>
     <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     appHeader: Header,
     appFooter: Footer
   }
-}
+};
 </script>
 <style>
-@import './assets/css/style.css';
+@import "./assets/css/style.css";
 </style>
